bd9709b408493a0810ca6df0c48984ec0f6eddb6
Fadli Ibnu Malik <fadliibnumalik@gmail.com>
2020-06-03 21:19:44 -0700

    merubah file home.php dan model_siswa_baru.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -15,6 +15,40 @@ class Home extends CI_Controller
         $data['sekolah'] = 'SMK MERAH PUTIH';
         $data['tbl_siswa_baru'] = $this->Model_siswa_baru->getAllSiswaBaru();
 
+        // $this->load->model('Model_siswa_baru', 'peoples');
+
+        // $data['peoples'] = $this->peoples->getAllPeoples();
+
+        // load library
+        $this->load->library('pagination');
+
+        // ambil data keyword
+        // if( $this->input->post('submit') ) {
+        //     $data['keyword'] = $this->input->post('keyword');
+        //     $this->session->set_userdata('keyword', $data['keyword']);
+        // } else {
+        //     $data['keyword'] = $this->session->userdata('keyword');
+        //     ;
+        // }
+
+        // config
+        // $this->db->like('name', $data['keyword']);
+        // $this->db->or_like('email', $data['keyword']);
+        // $this->db->from('peoples');
+        $config['total_rows'] = $this->db->count_all_results();
+        $data['total_rows'] = $config['total_rows'];
+        $config['per_page'] = 5;
+
+        // initialize
+        $this->pagination->initialize($config);
+
+        $data['start'] = $this->uri->segment(3);
+        // $data['tbl_siswa_baru'] = $this->tbl_siswa_baru->getPeoples($config['per_page'], $data['start'], $data['keyword']);
+        // $this->load->view('templates/header', $data);
+        // $this->load->view('peoples/index', $data);
+        // $this->load->view('templates/footer');
+
+
         $this->load->view('templates/header', $data);
         $this->load->view('home/index', $data);
         $this->load->view('templates/footer', $data);
--- application/models/Model_siswa_baru.php
+++ application/models/Model_siswa_baru.php
@@ -12,20 +12,25 @@ class Model_siswa_baru extends CI_Model
     public function getSiswaBaru()
     {
         return $this->db->get_where('tbl_siswa_baru', ['kode_pendaftaran' => $this->session->userdata('kode_pendaftaran')])->row_array();
-        // return $this->db->get_where('tbl_siswa_baru', ['kode_pendaftaran' => $kode_pendaftaran])->row_array();
     }
 
-    // public function getAgama()
+    // fitur untuk pencarian
+    // public function getPeoples($limit, $start, $keyword = null)
     // {
-    // $kp = htmlspecialchars($this->input->post('kode_pendaftaran', TRUE));
+    //     if( $keyword ) {
+    //         $this->db->like('name', $keyword);
+    //         $this->db->or_like('email', $keyword);
+    //     }
 
-    // $query = "SELECT `tbl_siswa_baru`.*, `tbl_agama`.`nama_agama`
-    //           FROM `tbl_siswa_baru` JOIN `tbl_agama`
-    //           ON `tbl_siswa_baru`.`kd_agama` = `tbl_agama`.`kd_agama`
-    //         ";
-    // return $this->db->query($query)->result_array();
-    // return $this->db->query($query, ['kode_pendaftaran' => $this->session->userdata('kode_pendaftaran')])->row_array();
+    //     return $this->db->get('peoples', $limit, $start)->result_array();
     // }
+
+    public function countAllSiswaBaru()
+    {
+        return $this->db->get('tbl_siswa_baru')->num_rows();
+    }
+
+
 
     public function checkLogin()
     {
@@ -203,61 +208,4 @@ class Model_siswa_baru extends CI_Model
         $this->db->update('tbl_siswa_baru', $data,);
     }
 
-    // public function loginSiswaBaru()
-    // {
-    //     $siswa_baru = $this->db->get_where('user', ['email' => $email])->row_array();
-    //     return $siswa_baru
-    // }
-
-    // function save($foto)
-    // {
-    //     $data = array(
-    //         'nim'           => $this->input->post('nim', TRUE),
-    //         'kd_agama'      => $this->input->post('agama', TRUE),
-    //         'nama'          => $this->input->post('nama', TRUE),
-    //         'tanggal_lahir' => $this->input->post('tanggal_lahir', TRUE),
-    //         'tempat_lahir'  => $this->input->post('tempat_lahir', TRUE),
-    //         'gender'        => $this->input->post('gender', TRUE),
-    //         'foto'          => $foto,
-    //         'id_rombel'     => $this->input->post('rombel', TRUE)
-    //     );
-    //     $this->db->insert($this->table, $data);
-
-    //     $tahun_akademik = $this->db->get_where('tbl_tahun_akademik', array('is_aktif' => 'y'))->row_array();
-
-    //     $history =  array(
-    //         'nim'                 =>  $this->input->post('nim', TRUE),
-    //         'id_tahun_akademik'   =>  $tahun_akademik['id_tahun_akademik'],
-    //         'id_rombel'           =>  $this->input->post('rombel', TRUE)
-    //     );
-    //     $this->db->insert('tbl_history_kelas', $history);
-    // }
-
-    // function update($foto)
-    // {
-    //     if (empty($foto)) {
-    //         // update without foto
-    //         $data = array(
-    //             'nama'          => $this->input->post('nama', TRUE),
-    //             'kd_agama'      => $this->input->post('agama', TRUE),
-    //             'tanggal_lahir' => $this->input->post('tanggal_lahir', TRUE),
-    //             'tempat_lahir'  => $this->input->post('tempat_lahir', TRUE),
-    //             'gender'        => $this->input->post('gender', TRUE)
-    //         );
-    //     } else {
-    //         // update with foto
-    //         $data = array(
-    //             'nama'          => $this->input->post('nama', TRUE),
-    //             'kd_agama'      => $this->input->post('agama', TRUE),
-    //             'tanggal_lahir' => $this->input->post('tanggal_lahir', TRUE),
-    //             'tempat_lahir'  => $this->input->post('tempat_lahir', TRUE),
-    //             'gender'        => $this->input->post('gender', TRUE),
-    //             'foto'          => $foto,
-    //             'id_rombel'     => $this->input->post('rombel', TRUE)
-    //         );
-    //     }
-    //     $nim   = $this->input->post('nim');
-    //     $this->db->where('nim', $nim);
-    //     $this->db->update($this->table, $data);
-    // }
 }
