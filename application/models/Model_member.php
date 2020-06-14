9fcc297b79bf1db952d701e85f88465cc1a7dcac
root <root@localhost.localdomain>
2020-06-14 21:55:29 +0700

    menambahkan halaman index member dan halaman lainnya

---
+++ application/models/Model_member.php
@@ -0,0 +1,196 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Model_member extends CI_Model
+{
+    // get data seluruh siswa baru
+    public function getAllMember()
+    {
+        return $this->db->get('tbl_member')->result_array();
+    }
+
+    public function getMember()
+    {
+        return $this->db->get_where('tbl_member', ['id_member' => $this->session->userdata('email')])->row_array();
+    }
+
+    // fitur untuk pagination
+    public function getMemberLimit($limit, $start, $keyword = null)
+    // public function getMemberLimit($limit, $start)
+    {
+        // untuk pencarian
+        if ($keyword) {
+            $this->db->like('id_member', $keyword);
+            $this->db->or_like('nama', $keyword);
+            $this->db->or_like('asal_sekolah', $keyword);
+            $this->db->or_like('alamat', $keyword);
+            $this->db->or_like('validasi', $keyword);
+        }
+        // elseif ( $keyword == '1' ) {
+        //     $this->db->like('is_active', $keyword);
+        // }
+
+        return $this->db->get('tbl_member', $limit, $start)->result_array();
+    }
+
+    public function countAllMember()
+    {
+        return $this->db->get('tbl_member')->num_rows();
+    }
+
+
+
+    public function checkLogin()
+    {
+        $email = htmlspecialchars($this->input->post('email', TRUE));
+        $password = htmlspecialchars($this->input->post('password', TRUE));
+
+        $member = $this->db->get_where('tbl_member', ['email' => $email])->row_array();
+
+        // var_dump($password);
+
+        // jika usernya ada
+        if ($member) {
+            // jika usernya aktif
+            // if ($member['is_active'] == 0) {
+            // cek password
+            if ($password == $member['id_member']) {
+                $data = [
+                    'email' => $member['email'],
+                    'role_id' => $member['role_id']
+                ];
+
+                $this->session->set_userdata($data);
+                // if ($member['role_id'] == 1) {
+                //     redirect('member');
+                // } else {
+                redirect('member');
+                // }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Password Anda Salah!</div>');
+                redirect('member/login');
+            }
+            // } else {
+            //     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Mohon maaf akun anda belum diaktivasi!</div>');
+            //     redirect('member/login');
+            // }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Email belum terdaftar</div>');
+            redirect('member/login');
+        }
+    }
+
+    public function tambahDataMember($idmember)
+    {
+
+        $nama = $this->input->post('nama', TRUE);
+        $email = $this->input->post('email', true);
+
+        $data = [
+            'id_member'             => ($idmember),
+            'nama'                  => htmlspecialchars($nama),
+            'tempat_lahir'          => htmlspecialchars($this->input->post('tempat_lahir', TRUE)),
+            'tanggal_lahir'         => htmlspecialchars($this->input->post('tanggal_lahir', TRUE)),
+            'jenis_kelamin'         => $this->input->post('jenis_kelamin', TRUE),
+            'kd_agama'              => $this->input->post('agama', TRUE),
+            'alamat'                => htmlspecialchars($this->input->post('alamat', TRUE)),
+            'rt'                    => htmlspecialchars($this->input->post('rt', TRUE)),
+            'rw'                    => htmlspecialchars($this->input->post('rw', TRUE)),
+            'kelurahan'             => htmlspecialchars($this->input->post('kelurahan', TRUE)),
+            'kecamatan'             => htmlspecialchars($this->input->post('kecamatan', TRUE)),
+            'no_hp'                 => htmlspecialchars($this->input->post('no_hp', TRUE)),
+            'pendidikan'         => htmlspecialchars($this->input->post('pendidikan', TRUE)),
+            'pekerjaan'          => htmlspecialchars($this->input->post('pekerjaan', TRUE)),
+
+            // 'name' => htmlspecialchars($this->input->post('name', true)),
+            'email' => htmlspecialchars($email),
+            'image' => 'default.png',
+            'password' => password_hash($idmember, PASSWORD_DEFAULT),
+            'role_id' => 2,
+            'is_active' => 1,
+            'date_created' => time()
+        ];
+        $this->db->insert('tbl_member', $data);
+    }
+
+    public function editDataMember()
+    {
+
+        $nama = $this->input->post('nama', TRUE);
+        $email = $this->input->post('email', true);
+        $idmember = $this->input->post('id_member', true);
+
+        $data = [
+            'id_member'      => ($idmember),
+            'nama'                  => htmlspecialchars($nama),
+            'tempat_lahir'          => htmlspecialchars($this->input->post('tempat_lahir', TRUE)),
+            'tanggal_lahir'         => htmlspecialchars($this->input->post('tanggal_lahir', TRUE)),
+            'jenis_kelamin'         => $this->input->post('jenis_kelamin', TRUE),
+            'kd_agama'              => $this->input->post('agama', TRUE),
+            'warganegara'           => $this->input->post('warganegara', TRUE),
+            'statussiswa'           => $this->input->post('statussiswa', TRUE),
+            'anak_ke'               => htmlspecialchars($this->input->post('anak_ke', TRUE)),
+            'dari__bersaudara'      => htmlspecialchars($this->input->post('dari__bersaudara', TRUE)),
+            'jumlah_saudara'        => htmlspecialchars($this->input->post('jumlah_saudara', TRUE)),
+            'alamat'                => htmlspecialchars($this->input->post('alamat', TRUE)),
+            'rt'                    => htmlspecialchars($this->input->post('rt', TRUE)),
+            'rw'                    => htmlspecialchars($this->input->post('rw', TRUE)),
+            'kelurahan'             => htmlspecialchars($this->input->post('kelurahan', TRUE)),
+            'kecamatan'             => htmlspecialchars($this->input->post('kecamatan', TRUE)),
+            'no_hp'                 => htmlspecialchars($this->input->post('no_hp', TRUE)),
+            'tinggalbersama'        => $this->input->post('tinggalbersama', TRUE),
+            'jarak'                 => htmlspecialchars($this->input->post('jarak', TRUE)),
+            'transport'             => $this->input->post('transport', TRUE),
+            'jurusan'               => $this->input->post('jurusan', TRUE),
+            'asal_sekolah'          => htmlspecialchars($this->input->post('asal_sekolah', TRUE)),
+            'nisn'                  => htmlspecialchars($this->input->post('nisn', TRUE)),
+            'no_sttb'               => htmlspecialchars($this->input->post('no_sttb', TRUE)),
+            'pindahan'              => htmlspecialchars($this->input->post('pindahan', TRUE)),
+            'suratpindah'           => $this->input->post('suratpindah', TRUE),
+            'alasan'                => htmlspecialchars($this->input->post('alasan', TRUE)),
+
+            // data orang tua siswa
+            'nama'               => htmlspecialchars($this->input->post('nama', TRUE)),
+            'alamat'             => htmlspecialchars($this->input->post('alamat', TRUE)),
+            'no_hp'              => htmlspecialchars($this->input->post('no_hp', TRUE)),
+            'pendidikan'         => htmlspecialchars($this->input->post('pendidikan', TRUE)),
+            'pekerjaan'          => htmlspecialchars($this->input->post('pekerjaan', TRUE)),
+            'penghasilan'        => htmlspecialchars($this->input->post('penghasilan', TRUE)),
+
+            // 'name' => htmlspecialchars($this->input->post('name', true)),
+            'email' => htmlspecialchars($email),
+            'password' => password_hash($this->input->post('tanggal_lahir'), PASSWORD_DEFAULT),
+            'role_id' => 2
+        ];
+
+        // cek jika ada gambar yang akan diupload
+        $upload_image = $_FILES['image']['name'];
+
+        if ($upload_image) {
+            $config['allowed_types'] = 'gif|jpg|jpeg|png';
+            $config['max_size']      = '2048';
+            $config['upload_path'] = './assets/img/profile/';
+
+            $this->load->library('upload', $config);
+
+            if ($this->upload->do_upload('image')) {
+                $old_image = $data['tbl_member']['image'];
+                if ($old_image != 'default.jpg') {
+                    unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                }
+                $new_image = $this->upload->data('file_name');
+                $this->db->set('image', $new_image);
+                // $this->Model_member->editDataMember($new_image);
+            } else {
+                echo $this->upload->dispay_errors();
+            }
+        }
+
+
+        // $this->db->set('name', $data);
+
+        // $this->db->set('image', $new_image);
+        $this->db->where('id_member', $idmember);
+        $this->db->update('tbl_member', $data,);
+    }
+}
---
+++ application/views/member/registration.php
@@ -0,0 +1,210 @@
+<div class="jumbotron daftar luhur">
+    <center><img src="<?= base_url(); ?>assets/img/logolibrary.jpg" alt="logo" width="100px" class="img img-responsive full-width rounded-circle mt-4"></center>
+    <div class="text-center">
+        <h2 style="color:rgb(252, 252, 252)">
+            <center><?= $judul; ?></center>
+        </h2>
+        <h1 style="color:rgb(252, 252, 252)">
+            <center><?= $navbar; ?></center>
+        </h1>
+    </div>
+</div>
+
+
+<div class="container-fluid mt-5">
+    <div class="row" style="margin-left:0;margin-right:0;">
+
+        <div class="col-md-8 mx-auto">
+            <div class="row bg-light mb-5">
+                <div class="col-lg">
+                    <div class="p-5">
+                        <div class="header">
+                            <h3 class="h3 text-gray-900 mb-lg-5">
+                                <center>Formulir Pendaftaran</center>
+                            </h3>
+                        </div>
+                        <?php
+                        if ($this->session->flashdata('message')) :
+                        ?>
+                            <div class="alert alert-success alert-dismissible fade show" role="alert">Selamat! Terima kasih anda sudah mendaftar di Perpustakaan Desa Ciwaru.<br>
+                                Silahkan login! dengan menggunakan Usename <strong>Email</strong> dan Password dengan menggunakan Id Member <strong><?= $this->session->flashdata('message'); ?></strong> !"
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        <?php endif; ?>
+
+                        <hr style="border:1px solid light;">
+
+                        <form class="user" method="post" action="">
+                            <h3 class="h5 text-gray-900 mt-sm-5 mb-sm-3">Keterangan Pribadi Member</h3>
+
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="nama">
+                                    Nama Lengkap
+                                </label>
+                                <div class="col-sm-7">
+                                    <input type="text" name="nama" placeholder="Masukkan Nama Lengkap" id="nama" class="form-control form-control-sm" value="<?= set_value('nama'); ?>">
+                                </div>
+                                <?= form_error('nama', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="tempat_lahir">
+                                    Tempat Tanggal Lahir
+                                </label>
+                                <div class="col-sm-4">
+                                    <input type="text" name="tempat_lahir" placeholder="Tempat Lahir" id="tempat_lahir" class="form-control form-control-sm" value="<?= set_value('tempat_lahir'); ?>">
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="date" name="tanggal_lahir" placeholder="Tanggal Lahir" id="tanggal_lahir" class="form-control form-control-sm" value="<?= set_value('tanggal_lahir'); ?>">
+                                </div>
+                                <?= form_error('tempat_lahir', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3 col-sm-4">', '</small>'); ?>
+                                <?= form_error('tanggal_lahir', '<small class="text-danger pl-3 col-sm-3 align-items-sm-end">', '</small>'); ?>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="gender">
+                                    Jenis Kelamin
+                                </label>
+                                <div class="col-sm-5">
+                                    <?php
+                                    $jenis_kelamin = array(
+                                        null => '- Silahkan Pilih -',
+                                        'L' => 'Laki-laki',
+                                        'P' => 'Perempuan'
+                                    );
+                                    $pilih = array(null);
+                                    echo form_dropdown(
+                                        'jenis_kelamin',
+                                        $jenis_kelamin,
+                                        $pilih,
+                                        "class='form-control form-control-sm'"
+                                    );
+                                    ?>
+                                </div>
+                                <?= form_error('jenis_kelamin', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3 col-sm-7">', '</small>'); ?>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="agama">
+                                    Agama
+                                </label>
+                                <div class="col-sm-5">
+                                    <?php
+                                    echo cmb_dinamis('agama', 'tbl_agama', 'nama_agama', 'kd_agama');
+                                    ?>
+                                </div>
+                                <?= form_error('agama', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3 col-sm-7">', '</small>'); ?>
+                            </div>
+
+                            <div class="form-group row mb-lg-5">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="email">
+                                    E-mail
+                                </label>
+                                <div class="col-sm-5">
+                                    <input type="text" class="form-control form-control-sm form-control form-control-sm" id="email" name="email" placeholder="E-mail" value="<?= set_value('email'); ?>">
+                                </div>
+                                <?= form_error('email', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div>
+                            <hr>
+
+                            <h3 class="h5 text-gray-900 mt-sm-5 mb-sm-3">Keterangan Tempat Tinggal Member</h3>
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="alamat">
+                                    Alamat
+                                </label>
+                                <div class="col-sm-7">
+                                    <textarea name="alamat" id="alamat" class="form-control form-control-sm" value="<?= set_value('alamat'); ?>"></textarea>
+                                </div>
+                                <?= form_error('alamat', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="rt">
+                                    RT / RW
+                                </label>
+                                <div class="col-sm-3">
+                                    <input type="text" name="rt" placeholder="RT" id="rt" class="form-control form-control-sm" value="<?= set_value('rt'); ?>">
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="text" name="rw" placeholder="RW" id="rw" class="form-control form-control-sm" value="<?= set_value('rw'); ?>">
+                                </div>
+                                <?= form_error('rt', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3 col-sm-3">', '</small>'); ?>
+                                <?= form_error('rw', '<small class="text-danger pl-3 col-sm-3 align-items-sm-end">', '</small>'); ?>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="kelurahan">
+                                    Kelurahan / Desa
+                                </label>
+                                <div class="col-sm-7">
+                                    <input type="text" name="kelurahan" placeholder="Kelurahan / Desa" id="kelurahan" class="form-control form-control-sm" value="<?= set_value('kelurahan'); ?>">
+                                </div>
+                                <?= form_error('kelurahan', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="kecamatan">
+                                    Kecamatan
+                                </label>
+                                <div class="col-sm-7">
+                                    <input type="text" name="kecamatan" placeholder="Kecamatan" id="kecamatan" class="form-control form-control-sm" value="<?= set_value('kecamatan'); ?>">
+                                </div>
+                                <?= form_error('kecamatan', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="no_hp">
+                                    No. HP
+                                </label>
+                                <div class="col-sm-7">
+                                    <input type="text" name="no_hp" placeholder="Nomor HP" id="no_hp" class="form-control form-control-sm" value="<?= set_value('no_hp'); ?>">
+                                </div>
+                                <?= form_error('no_hp', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="pendidikan">
+                                    Pendidikan Terakhir
+                                </label>
+                                <div class="col-sm-7">
+                                    <input type="text" name="pendidikan" placeholder="Pendidikan Terakhir" id="pendidikan" class="form-control form-control-sm" value="<?= set_value('pendidikan'); ?>">
+                                </div>
+                                <?= form_error('pendidikan', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div>
+
+                            <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="pekerjaan">
+                                    Pekerjaan
+                                </label>
+                                <div class="col-sm-7">
+                                    <input type="text" name="pekerjaan" placeholder="Pekerjaan" id="pekerjaan" class="form-control form-control-sm" value="<?= set_value('pekerjaan'); ?>">
+                                </div>
+                                <?= form_error('pekerjaan', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div>
+
+
+                            <!-- <div class="form-group row">
+                                <label class="col-sm-5 col-form-label col-form-label-sm" for="password1">
+                                    Password
+                                </label>
+                                <div class="col-sm-3 mb-3 mb-sm-0">
+                                    <input type="password" class="form-control form-control-sm form-control form-control-sm" id="password1" name="password1" placeholder="Password">
+                                </div>
+                                <div class="col-sm-3">
+                                    <input type="password" class="form-control form-control-sm form-control form-control-sm" id="password2" name="password2" placeholder="Repeat Password">
+                                </div>
+                                <?= form_error('password1', '<div class="col-sm-5"></div><small class="text-danger mt-sm-1 pl-3">', '</small>'); ?>
+                            </div> -->
+                            <div class="text-center mt-lg-5">
+                                <button type="submit" class="btn btn-success btn-block btn-user">
+                                    Daftar
+                                </button>
+                            </div>
+                        </form>
+                        <hr>
+                        <div class="text-center">
+                            <a class="text-primary" href="<?= base_url('member/login'); ?>">Sudah Daftar? Silahkan Login!</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
